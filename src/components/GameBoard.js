f29167e825a11992ab8aed6072895f457ff680b5
Catherine <cmlavin7@gmail.com>
2017-08-18 17:01:39 -0400

    added click handler

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -6,14 +6,25 @@ class GameBoard extends React.Component{
   constructor(){
     super()
     this.state = {
-      questions: []
+      questions: [],
+      randVals: [],
+      display: [],
+      questionIndex: 10,
+      questionInSession: false
     }
+    this.checkAnswer = this.checkAnswer.bind(this)
   }
 
 
 componentWillReceiveProps(nextProps){
   this.setState({
-    questions: nextProps
+    questions: nextProps,
+  }, () => {
+    let vals = this.generateValues()
+    this.setState({
+      randVals: vals,
+      display: vals
+    })
   })
 }
 
@@ -23,13 +34,53 @@ componentWillReceiveProps(nextProps){
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
+generateValues(){
+    var randVals = this.state.questions.gameQuestions.map((question) => this.getRandomIntInclusive(0, 500))
+    console.log(randVals)
+    return randVals
+}
+
+showQuestion = (event) => {
+  if(this.state.questionInSession === false){
+  let id = parseInt(event.target.parentElement.id)
+  let disp = this.state.display
+  disp[id] = this.state.questions.gameQuestions[id][0]
+  this.setState({
+    display: disp,
+    questionInSession: true,
+    questionIndex: id
+  })}
+}
+
+handleSubmit = (event) => {
+  this.setState({
+    questionInSession: false
+  })
+  let input = document.getElementById('input').value.toLowerCase()
+  let id = this.state.questionIndex
+  this.checkAnswer(input, id)
+}
+
+checkAnswer = (input, id) => {
+  let answer = this.state.questions.gameQuestions[id][1].toLowerCase()
+  if(answer === input){
+
+  }else if(answer !== input){
+    
+  }
+}
+
 render(){
-  //debugger
   return(
     <div className="ui grid">
-      {this.state.questions.length !== 0 && this.state.questions.gameQuestions.map((question, index) => {
-        return <GameTile question={question} value={this.getRandomIntInclusive}/>
+      {this.state.questions.length !== 0 && this.state.display.map((val, index) => {
+        return <GameTile display={val} index={index} showQuestion={this.showQuestion}/>
       })}
+      {this.state.questionInSession &&
+        <div>
+         <input id="input" type="text"/>
+        <button onClick={this.handleSubmit}>Submit</button>
+      </div>}
     </div>
   )}
 }
--- src/components/GameTile.js
+++ src/components/GameTile.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {Grid, Segment} from 'semantic-ui-react'
 
 const GameTile = (props) => {
-  
+
   return(
     <div className="five wide column" >
-      <p>{props.question}</p>
-      <p>{props.value(0, 500)}</p>
+    <div id={props.index}>
+      <h3 onClick={props.showQuestion}>{props.display}</h3>
+    </div>
     </div>
   )
 }
